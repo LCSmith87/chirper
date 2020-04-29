bbb5257a375555667d44234a229422407f43bcb8
LCSmith87 <LCSmith87@gmail.com>
2020-04-29 10:50:03 -0500

    Finished chirper project from Udacity React Nanodegree

---
+++ src/components/NewTweet.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
+
+class NewTweet extends Component {
+    state = {
+        text: '',
+
+    }
+    handleChange = (e) => {
+        const text = e.target.value
+
+        this.setState(() => ({
+            text
+        }))
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const { text } = this.state
+        const { dispatch, id } = this.props
+
+        dispatch(handleAddTweet(text, id))
+
+        this.setState(() => ({
+            text: '',
+            toHome: id ? false : true,
+        }))
+    }
+    render() {
+        const { text, toHome } = this.state
+        const tweetLeft = 280 - text.length
+
+        if (toHome === true) {
+            return <Redirect to='/' />
+        }
+
+        return(
+            <div>
+                <h3 className='center'>Compose New Tweet</h3>
+                <form className='new-tweet' onSubmit={this.handleSubmit}>
+                    <textarea
+                        placeholder="What's Happening?"
+                        value={text}
+                        onChange={this.handleChange}
+                        className='textarea'
+                        maxLength={280}
+                    />
+                    <div className={tweetLeft <= 100 ? 'tweet-length red' : 'tweet-length'}>
+                        {tweetLeft}
+                    </div>
+                    <button
+                        className='btn'
+                        type='submit'
+                        disabled={text === ''}>
+                            Submit
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect()(NewTweet);
